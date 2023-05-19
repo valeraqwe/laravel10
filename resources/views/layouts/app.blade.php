0745e11a4dc75d42480497aed888488bbe16b379
valeraqwe <valera.komar.1999@gmail.com>
2023-05-19 16:40:04 +0300

    change search title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -62,7 +62,7 @@
                 <form method="get" action="{{route('search')}}">
                     <input name="q" value="{{request()->get('q')}}"
                            class="block w-full rounded-md border-0 px-3.5 py-2 t0ext-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6 font-medium"
-                           placeholder="Type an hit enter to search anything"/>
+                           placeholder="Type and hit enter to search"/>
                 </form>
                 @auth
                     <div class="flex sm:items-center sm:ml-6">
