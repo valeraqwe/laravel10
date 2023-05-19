b4c45cecf90a6be79b64c7e001d00ad70dcbe9e3
valeraqwe <valera.komar.1999@gmail.com>
2023-05-19 16:50:55 +0300

    change footer title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -120,7 +120,7 @@
 
 <footer class="w-full border-t bg-white pb-12">
     <div class="w-full container mx-auto flex flex-col items-center">
-        <div class="uppercase py-6">&copy; myblog.com</div>
+        <div class="uppercase py-6">&copy; komar-nm.net.ua 2023</div>
     </div>
 </footer>
 
