698065802e2ac7262e6311ef0f424fd956fdc72e
valeraqwe <valera.komar.1999@gmail.com>
2023-05-20 09:23:11 +0300

    fixes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,49 +36,36 @@
 
 
 <!-- Topic Nav -->
-<nav class="w-full py-4 border-t border-b bg-gray-100" x-data="{ open: false }">
+<nav class="w-full py-6 border-t border-b bg-gray-100" x-data="{ open: false }">
     <div class="block sm:hidden">
-        <a
-            href="#"
-            class="block md:hidden text-base font-bold uppercase text-center flex justify-center items-center"
-            @click="open = !open"
-        >
+        <a href="#" class="block md:hidden text-base font-bold uppercase text-center flex justify-center items-center" @click="open = !open">
             Topics <i :class="open ? 'fa-chevron-down': 'fa-chevron-up'" class="fas ml-2"></i>
         </a>
     </div>
     <div :class="open ? 'block': 'hidden'" class="w-full flex-grow sm:flex sm:items-center sm:w-auto">
-        <div
-            class="w-full container mx-auto flex flex-col sm:flex-row items-center justify-between text-sm font-bold uppercase mt-0 px-6 py-2">
-            <div>
-                <a href="{{route('home')}}" class="hover:bg-blue-600 hover:text-white rounded py-2 px-4 mx-2">Home</a>
+        <div class="w-full container mx-auto flex flex-col sm:flex-row items-center justify-between text-sm font-bold uppercase mt-0 px-6 py-4">
+            <div class="my-2">
+                <a href="{{route('home')}}" class="hover:bg-blue-600 hover:text-white rounded py-3 px-4 mx-4">Home</a>
                 @foreach($categories as $category)
-                    <a href="{{route('by-category', $category)}}"
-                       class="hover:bg-blue-600 hover:text-white rounded py-2 px-4 mx-2">{{$category->title}}</a>
+                    <a href="{{route('by-category', $category)}}" class="hover:bg-blue-600 hover:text-white rounded py-3 px-4 mx-4">{{$category->title}}</a>
                 @endforeach
-                <a href="{{route('about-us')}}" class="hover:bg-blue-600 hover:text-white rounded py-2 px-4 mx-2">About
-                    </a>
+                <a href="{{route('about-author')}}" class="hover:bg-blue-600 hover:text-white rounded py-3 px-4 mx-4">About</a>
             </div>
 
             <div class="flex items-center">
                 <form method="get" action="{{route('search')}}">
-                    <input name="q" value="{{request()->get('q')}}"
-                           class="block w-full rounded-md border-0 px-3.5 py-2 t0ext-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6 font-medium"
-                           placeholder="Type to search..."/>
+                    <input name="q" value="{{request()->get('q')}}" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6 font-medium" placeholder="Type to search..." />
                 </form>
                 @auth
                     <div class="flex sm:items-center sm:ml-6">
                         <x-dropdown align="right" width="48">
                             <x-slot name="trigger">
-                                <button
-                                    class="hover:bg-blue-600 hover:text-white flex items-center rounded py-2 px-4 mx-2">
+                                <button class="hover:bg-blue-600 hover:text-white flex items-center rounded py-3 px-4 mx-2">
                                     <div>{{ Auth::user()->name }}</div>
 
                                     <div class="ml-1">
-                                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
-                                             viewBox="0 0 20 20">
-                                            <path fill-rule="evenodd"
-                                                  d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                                  clip-rule="evenodd"/>
+                                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
                                         </svg>
                                     </div>
                                 </button>
