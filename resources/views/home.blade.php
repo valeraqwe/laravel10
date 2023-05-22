d9191ec521ba2f6ad4aa649822ec15f9ec65acd5
valeraqwe <valera.komar.1999@gmail.com>
2023-05-22 15:58:46 +0300

    add emoji

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
             <!-- Latest Post -->
             <div class="col-span-2">
                 <h2 class="text-lg sm:text-xl font-bold text-blue-500 uppercase pb-1 border-b-2 border-blue-500 mb-3">
-                    Latest Post
+                    Latest Post ✍️
                 </h2>
 
                 @if ($latestPost)
@@ -17,7 +17,7 @@
             <!-- Popular 3 post -->
             <div>
                 <h2 class="text-lg sm:text-xl font-bold text-blue-500 uppercase pb-1 border-b-2 border-blue-500 mb-3">
-                    Popular Posts
+                    Popular Posts ✍️
                 </h2>
                 @foreach($popularPosts as $post)
                     <div class="grid grid-cols-4 gap-2 mb-4">
@@ -50,7 +50,7 @@
         <!-- Recommended posts -->
         <div class="mb-8">
             <h2 class="text-lg sm:text-xl font-bold text-blue-500 uppercase pb-1 border-b-2 border-blue-500 mb-3">
-                Recommended Posts
+                Recommended Posts ✍️
             </h2>
 
             <div class="grid grid-cols-1 md:grid-cols-3 gap-3">
