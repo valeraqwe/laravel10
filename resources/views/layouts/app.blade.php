6267581515520db33097325bbaa40ee6f1dc60f8
valeraqwe <valera.komar.1999@gmail.com>
2023-05-22 11:12:33 +0300

    fix layout design

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,82 +37,97 @@
     </div>
 </header>
 
+<!-- Nav -->
+<nav class="w-full items-center py-6 border-t border-b bg-gray-100" x-data="{ open: false }">
+    <div class="w-full container mx-auto flex flex-col sm:flex-row items-center justify-center text-sm font-bold uppercase mt-0 px-6 py-4">
+        <div class="my-2 flex items-center justify-center">
+            <a href="{{route('home')}}" class="hover:bg-blue-600 hover:text-white rounded py-3 px-4 mx-4">Home</a>
 
-<!-- Topic Nav -->
-<nav class="w-full py-6 border-t border-b bg-gray-100" x-data="{ open: false }">
-    <div class="block sm:hidden">
-        <a href="#" class="block md:hidden text-base font-bold uppercase text-center flex justify-center items-center" @click="open = !open">
-            Topics <i :class="open ? 'fa-chevron-down': 'fa-chevron-up'" class="fas ml-2"></i>
-        </a>
-    </div>
-    <div :class="open ? 'block': 'hidden'" class="w-full flex-grow sm:flex sm:items-center sm:w-auto">
-        <div class="w-full container mx-auto flex flex-col sm:flex-row items-center justify-between text-sm font-bold uppercase mt-0 px-6 py-4">
-            <div class="my-2">
-                <a href="{{route('home')}}" class="hover:bg-blue-600 hover:text-white rounded py-3 px-4 mx-4">Home</a>
+            <div class="relative sm:hidden">
+                <button @click="open = !open" class="hover:bg-blue-600 hover:text-white rounded py-3 px-4 mx-4">
+                    Categories <i :class="open ? 'fa-chevron-up': 'fa-chevron-down'" class="fas ml-2"></i>
+                </button>
+                <div x-show="open" @click.away="open = false" class="absolute z-10 w-40 bg-white rounded shadow-lg mt-2">
+                    @foreach($categories as $category)
+                        <a href="{{route('by-category', $category)}}" class="block px-4 py-2 text-gray-800 hover:bg-gray-200">{{$category->title}}</a>
+                    @endforeach
+                </div>
+            </div>
+
+            <div class="hidden sm:flex">
                 @foreach($categories as $category)
                     <a href="{{route('by-category', $category)}}" class="hover:bg-blue-600 hover:text-white rounded py-3 px-4 mx-4">{{$category->title}}</a>
                 @endforeach
-                <a href="{{route('about-author')}}" class="hover:bg-blue-600 hover:text-white rounded py-3 px-4 mx-4">About</a>
             </div>
 
-            <div class="flex items-center">
-                <form method="get" action="{{route('search')}}">
-                    <input name="q" value="{{request()->get('q')}}" class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6 font-medium" placeholder="Type to search..." />
-                </form>
-                @auth
-                    <div class="flex sm:items-center sm:ml-6">
-                        <x-dropdown align="right" width="48">
-                            <x-slot name="trigger">
-                                <button class="hover:bg-blue-600 hover:text-white flex items-center rounded py-3 px-4 mx-2">
-                                    <div>{{ Auth::user()->name }}</div>
-
-                                    <div class="ml-1">
-                                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                        </svg>
-                                    </div>
-                                </button>
-                            </x-slot>
-
-                            <x-slot name="content">
-                                <x-dropdown-link :href="route('profile.edit')">
-                                    {{ __('Profile') }}
-                                </x-dropdown-link>
-
-                                <!-- Authentication -->
-                                <form method="POST" action="{{ route('logout') }}">
-                                    @csrf
-
-                                    <x-dropdown-link :href="route('logout')"
-                                                     onclick="event.preventDefault();
-                                                this.closest('form').submit();">
-                                        {{ __('Log Out') }}
-                                    </x-dropdown-link>
-                                </form>
-                            </x-slot>
-                        </x-dropdown>
-                    </div>
-                @else
-                    <a href="{{route('login')}}"
-                       class="hover:bg-blue-600 hover:text-white rounded py-2 px-4 mx-2">Login</a>
-                    <a href="{{route('register')}}" class="bg-blue-600 text-white rounded py-2 px-4 mx-2">Register</a>
-                @endauth
-            </div>
+            <a href="{{route('about-author')}}" class="hover:bg-blue-600 hover:text-white rounded py-3 px-4 mx-4">About</a>
         </div>
     </div>
 </nav>
 
-
+<!-- Site Content -->
 <div class="container mx-auto py-6">
+    <!-- Search, Login, and Register -->
+    <div class="flex items-center justify-center sm:justify-end mt-4 sm:mt-0">
+        <form method="get" action="{{ route('search') }}">
+            <input name="q" value="{{ request()->get('q') }}"
+                   class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6 font-medium"
+                   placeholder="Type to search..."/>
+        </form>
+        @auth
+            <div class="flex sm:items-center sm:ml-6">
+                <x-dropdown align="right" width="48">
+                    <x-slot name="trigger">
+                        <button class="hover:bg-blue-600 hover:text-white flex items-center rounded py-3 px-4 mx-2">
+                            <div>{{ Auth::user()->name }}</div>
+                            <div class="ml-1">
+                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
+                                     viewBox="0 0 20 20">
+                                    <path fill-rule="evenodd"
+                                          d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                          clip-rule="evenodd"/>
+                                </svg>
+                            </div>
+                        </button>
+                    </x-slot>
 
+                    <x-slot name="content">
+                        <x-dropdown-link :href="route('profile.edit')">
+                            {{ __('Profile') }}
+                        </x-dropdown-link>
+
+                        <!-- Authentication -->
+                        <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+
+                            <x-dropdown-link :href="route('logout')"
+                                             onclick="event.preventDefault();
+                                     this.closest('form').submit();">
+                                {{ __('Log Out') }}
+                            </x-dropdown-link>
+                        </form>
+                    </x-slot>
+                </x-dropdown>
+            </div>
+        @else
+            <div class="flex items-center mt-4 sm:mt-0">
+                <a href="{{ route('login') }}"
+                   class="hover:bg-blue-600 hover:text-white rounded py-1 px-2 sm:px-4 mx-2">Login</a>
+                <a href="{{ route('register') }}" class="bg-blue-600 text-white rounded py-1 px-2 sm:px-4 mx-2">Register</a>
+            </div>
+        @endauth
+
+    </div>
+
+    <!-- Site Content -->
     {{ $slot }}
-
 </div>
+
 
 <footer class="w-full border-t bg-white pb-12">
     <div class="w-full container mx-auto flex flex-col items-center">
         <div class="uppercase py-6">&copy; komar-nm.net.ua 2023</div>
-        <p>Made with 💚 by  Valeriy Komar</p>
+        <p>Made with 💚 by Valeriy Komar</p>
     </div>
 </footer>
 
