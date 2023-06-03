07b922156a4ec6f3a28bbe1c867dc9768167d35f
valeraqwe <valera.komar.1999@gmail.com>
2023-06-03 17:04:03 +0300

    fixes june 03

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -2,7 +2,7 @@
 <aside class="hidden md:flex md:w-1/3 flex-col items-center px-3">
 
     <div class="w-full bg-white shadow flex flex-col my-4 p-6">
-        <h3 class="text-xl font-semibold mb-3">All Categories
+        <h3 class="text-xl font-semibold mb-3">Всі категорії:
         </h3>
         @foreach($categories as $category)
             <a href="{{route('by-category', $category)}}"
@@ -20,7 +20,7 @@
         {!! \App\Models\TextWidget::getContent('about-us-sidebar') !!}
         <a href="{{route('about-author')}}"
            class="w-full bg-blue-800 text-white font-bold text-sm uppercase rounded hover:bg-blue-700 flex items-center justify-center px-2 py-3 mt-4">
-            Get to know about author
+            Дізнайтеся про автора
         </a>
     </div>
 </aside>
